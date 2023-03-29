98daffe8d0970b272049ec9dfed592042569ed05
Rychard-Famichou <127249588+Rychard-Famichou@users.noreply.github.com>
2023-03-29 13:33:22 +0200

    HomeWork 23.03.2023

---
+++ A2Calculus_systems.java
@@ -0,0 +1,79 @@
+package HomeWork;
+
+class A2Calculus_systems {
+    public static void main(String[] args) {
+
+        System.out.println("Task 1");
+        // Given
+        int a = 637;
+        System.out.print("DECIMAL = ");
+        System.out.println(a);
+        // Convert from decimal to binary
+        // Divide to zero
+        System.out.println(a / 2);
+        System.out.println(318 / 2);
+        System.out.println(159 / 2);
+        System.out.println(79 / 2);
+        System.out.println(39 / 2);
+        System.out.println(19 / 2);
+        System.out.println(9 / 2);
+        System.out.println(4 / 2);
+        System.out.println(2 / 2);
+        System.out.println(1 / 2);
+        // Collecting leftovers
+        System.out.print(a % 2);
+        System.out.print(318 % 2);
+        System.out.print(159 % 2);
+        System.out.print(79 % 2);
+        System.out.print(39 % 2);
+        System.out.print(19 % 2);
+        System.out.print(9 % 2);
+        System.out.print(4 % 2);
+        System.out.print(2 % 2);
+        System.out.println(1 % 2);
+        // Recording the desired opposite
+        int a16 = 1001111101;
+        System.out.print("BINARY = ");
+        System.out.println(a16);
+
+        System.out.println();
+        System.out.println("Task 2");
+        // Given
+        int b = 11100111;
+        System.out.print("BINARY = ");
+        System.out.println(b);
+        // Convert from binary to decimal
+        System.out.print("DECIMAL = ");
+        System.out.println(1*Math.pow(2,0) + 1*Math.pow(2,1) + 1*Math.pow(2,2) + 0 + 0 + 1*Math.pow(2,5) + 1*Math.pow(2,6) + 1*Math.pow(2,7));
+
+        System.out.println();
+        System.out.println("Task 3");
+        // Given
+        int c = 637;
+        System.out.print("DECIMAL = ");
+        System.out.println(c);
+        // Convert from decimal to ternary
+        // Divine to zero
+        System.out.println(c / 3);
+        System.out.println(212 / 3);
+        System.out.println(70 / 3);
+        System.out.println(23 / 3);
+        System.out.println(7 / 3);
+        System.out.println(2 / 3);
+        // Collecting leftovers
+        System.out.print(c % 3);
+        System.out.print(212 % 3);
+        System.out.print(70 % 3);
+        System.out.print(23 % 3);
+        System.out.print(7 % 3);
+        System.out.println(2 % 3);
+        // Recording the desired opposite
+        int c3 = 212121;
+        System.out.print("Ternary = ");
+        System.out.println(c3);
+
+        System.out.println();
+        System.out.println("The end");
+
+    }
+}
