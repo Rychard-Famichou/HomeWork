344765903a0c0dc44df4571cf937f8b1cb858c83
Rychard-Famichou <127249588+Rychard-Famichou@users.noreply.github.com>
2023-03-30 22:29:38 +0200

    HomeWork 30.03.2023

---
+++ A4Char.java
@@ -0,0 +1,28 @@
+package HomeWork;
+
+public class A4Char {
+    public static void main(String[] args) {
+        System.out.println("Task 1");
+        // Given symbol
+        char q = '9';
+        System.out.println("Symbol: " + q);
+        // Method .value0f() class String & method .parseInt() class Integer
+        int qInt = Integer.parseInt(String.valueOf(q));
+        // OutputCheck
+        System.out.println("Convert to Integer, + 1 for check: " + (qInt + 1));
+
+        System.out.println();
+
+        System.out.println("Tack 2");
+        // Given
+        char w = '9';
+        System.out.println("Symbol: " + w);
+        // Method .getNumericValue() class Character
+        int wInt = Character.getNumericValue(w);
+        // OutputCheck
+        System.out.println("Convert to Integer, - 1 for check: " + (wInt - 1));
+
+        System.out.println();
+        System.out.println("The end");
+    }
+}
